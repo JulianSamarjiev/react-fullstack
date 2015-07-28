e21f248cadf8606b1bf597d967bc011760d44fd4
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-07-28 22:56:04 +0200

    semicolon added

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
-import FooterStore from '../stores/FooterStore';
+import FooterStore from '../stores/FooterStore'
 import FooterActions from '../actions/FooterActions';
 
 class Footer extends React.Component {
